d1f8ee07a4cd52271856b24c11e3c992908b00d4
Longo02 <leandrolongo0045@gmail.com>
2024-10-12 17:14:39 -0300

    add update funtions

--- configs/routes.js
+++ configs/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { getAllMovies, postMovie } = require('../controllers/movies');
+const { getAllMovies, postMovie, updateMovie } = require('../controllers/movies');
 
 const router = express.Router();
 
 
 router.get('/movies', getAllMovies)
 router.post('/movies', postMovie);
-
+router.put("/movies/:id", updateMovie)
 
 
 module.exports = router;
