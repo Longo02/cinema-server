cef8dadbf8609a58556ec35e0a8c2593df6d8a7c
Diego <wcproxd@gmail.com>
2024-10-12 17:41:51 -0300

    Add update movie

--- configs/routes.js
+++ configs/routes.js
@@ -4,9 +4,13 @@ const { getAllMovies, postMovie, updateMovie } = require('../controllers/movies'
 const router = express.Router();
 
 
-router.get('/movies', getAllMovies)
+router.get('/movies', getAllMovies);
 router.post('/movies', postMovie);
-router.put("/movies/:id", updateMovie)
+router.put('/movie/:id', updateMovie);
+
+
+//router.getBy('/movie/:', getBy);
+//router.delete('/movie/:id', deleteMovie);
 
 
 module.exports = router;
