97f6f3d36cf61b025c4b832923ddd8d43927b391
Longo02 <leandrolongo0045@gmail.com>
2024-10-29 15:11:38 -0300

    feature/filter-movie

--- configs/routes.js
+++ configs/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllMovies, postMovie, updateMovie, deleteMovie } = require('../controllers/movies');
+const { getAllMovies, postMovie, updateMovie, deleteMovie, filterGenre } = require('../controllers/movies');
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get('/movies', getAllMovies);
 router.post('/movies', postMovie);
 router.put('/movie/:id', updateMovie);
 router.delete("/movie/:id", deleteMovie);
+router.get("/movie/:genre", filterGenre ) ;
 
 //router.getBy('/movie/:', getBy);
 //router.delete('/movie/:id', deleteMovie);
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./configs/routes.js');
 const connectDB = require('./configs/db.js');
-
 
 const app = express()
 const PORT = process.env.PORT || 3000
